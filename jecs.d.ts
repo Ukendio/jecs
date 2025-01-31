5c19a3b595c04ec5de413bfd0b706da7fff707c3
maeriil <104389763+maeriil@users.noreply.github.com>
2025-01-31 09:29:32 -0500

    Typescript Types for Archetypes
    * resolves #182 - TS types for Archetypes
    
    * description for archetype query
    
    * updated archetype number type

--- jecs.d.ts
+++ jecs.d.ts
@@ -40,6 +40,23 @@ type FlattenTuple<T extends unknown[]> = T extends [infer U] ? U : LuaTuple<T>;
 type Nullable<T extends unknown[]> = { [K in keyof T]: T[K] | undefined };
 type InferComponents<A extends Id[]> = { [K in keyof A]: InferComponent<A[K]> };
 
+type ArchetypeId = number
+type Column = unknown[];
+
+type ArchetypeRecord = {
+    count: number;
+    column: number;
+}
+
+export type Archetype = {
+    id: number;
+    types: number[];
+    type: string;
+    entities: number[];
+    columns: Column[];
+    records: ArchetypeRecord[];
+}
+
 type Iter<T extends unknown[]> = IterableFunction<LuaTuple<[Entity, ...T]>>;
 
 export type CachedQuery<T extends unknown[]> = {
@@ -47,6 +64,12 @@ export type CachedQuery<T extends unknown[]> = {
 	 * Returns an iterator that produces a tuple of [Entity, ...queriedComponents].
 	 */
 	iter(): Iter<T>;
+
+	/**
+	 * Returns the matched archetypes of the query
+	 * @returns An array of archetypes of the query
+	 */
+	archetypes(): Archetype[];
 } & Iter<T>;
 
 export type Query<T extends unknown[]> = {
@@ -75,6 +98,12 @@ export type Query<T extends unknown[]> = {
 	 * @returns A new Query with the exclusion applied.
 	 */
 	without(...components: Id[]): Query<T>;
+
+	/**
+	 * Returns the matched archetypes of the query
+	 * @returns An array of archetypes of the query
+	 */
+	archetypes(): Archetype[];
 } & Iter<T>;
 
 export class World {
